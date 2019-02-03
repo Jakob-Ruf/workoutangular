99db2453c45449e4ec39014da4a84d71aa3d0f86
Jakob Ruf <jakob.ruf@cbs-consulting.de>
2019-02-03 20:50:03 +0100

    Serializer: Add deserialization for rounds

--- src/app/serializer.service.ts
+++ src/app/serializer.service.ts
@@ -3,6 +3,7 @@ import { Workout } from './classes/Workout';
 import { Round } from './classes/Round';
 import { Pause } from './classes/Pause';
 import { Exercise } from './classes/Exercise';
+import { Action } from './classes/Action';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,7 @@ export class SerializerService {
    * no JSON.parse since that loses prototype functions
    * @param input serialized workout string
    */
-  deserialize (input: string): Workout {
+  deserializeWorkout (input: string): Workout {
     try {
       const oWorkout = JSON.parse(input);
       const aRounds = [];
@@ -35,6 +36,18 @@ export class SerializerService {
     } catch (err) {
       return new Workout('ERROR DESERIZALIZING WORKOUT', []);
     }
+  }
+
+  deserializeRound (input: string): Round {
+    const round = JSON.parse(input);
+    const actions: Action[] = [];
+    for (const action of round.exercises) {
+      actions.push(
+        action.isPause ?
+        new Pause(action.duration) :
+        new Exercise(action.name, action.duration, action.image, action.muscles, action.alternatives));
+    }
+    return new Round(actions);
   }
 
   serialize (input: Workout): string {
