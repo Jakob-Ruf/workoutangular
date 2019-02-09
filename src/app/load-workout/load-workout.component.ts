56bb58f23440b016b7c5f66e2023cc0352346e9a
jakobruf <jakob.ruf93@gmail.com>
2019-02-09 09:19:44 +0100

    :sparkles: Add feature for model to load saved workouts

--- src/app/load-workout/load-workout.component.ts
+++ src/app/load-workout/load-workout.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ModelService } from '../model.service';
+import { Workout } from '../classes/Workout';
 
 @Component({
-  selector: 'app-load-workout',
-  templateUrl: './load-workout.component.html',
-  styleUrls: ['./load-workout.component.less']
+	selector: 'app-load-workout',
+	templateUrl: './load-workout.component.html',
+	styleUrls: ['./load-workout.component.less']
 })
 export class LoadWorkoutComponent implements OnInit {
+	workouts: Workout[] = [];
 
-  constructor() { }
+	constructor(private model: ModelService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.workouts = this.model.getSavedWorkouts();
+	}
 
 }
--- src/app/model.service.ts
+++ src/app/model.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
-import {Workout} from './classes/Workout';
+import { Workout } from './classes/Workout';
+import * as staticData from '../assets/data.json';
+import { SerializerService } from './serializer.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModelService {
+
   workout: Workout;
   constructor() {}
+
   getWorkout () {
     return this.workout || new Workout('NEW', []);
   }
+
   setWorkout (workout: Workout) {
     this.workout = workout;
+  }
+
+  getSavedWorkouts () {
+    const serializer = new SerializerService();
+    let workout: Workout;
+    let workoutTemp: any;
+    let workoutString: string;
+    const workouts: Workout[] = [];
+    for (workoutTemp of staticData.workouts) {
+      workoutString = JSON.stringify(workoutTemp);
+      workout = serializer.deserializeWorkout(workoutString);
+      workouts.push(workout);
+    }
+    return workouts;
   }
 }
