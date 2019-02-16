6fa2ad03262c2d23465dc24be9238ed2749c7337
jakobruf <jakob.ruf93@gmail.com>
2019-02-16 08:59:24 +0100

    Change navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModelService } from './model.service';
+import { Router } from '@angular/router';
+import { Constants } from './Constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fullcontrol';
   activeNav: number = 0;
+  model = new ModelService();
+  router: any = null;
+  constants: any = null;
 
+  ngOnInit () {
+    this.activeNav = this.model.getActiveNav() || 2;
+    this.router = Router;
+    this.constants = new Constants();
+  }
+
+  setNav (number) {
+    this.model.setActiveNav(number);
+    this.activeNav = number;
+  }
+
+  getRoute (name: string) {
+    return this.constants.get(name);
+  }
 }
