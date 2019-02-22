ba8669f4df50b4073024bdb966a4ea38df0a0d05
jakobruf <jakob.ruf93@gmail.com>
2019-02-22 11:56:36 +0100

    Workout loading preliminary logic

--- src/app/load-workout/load-workout.component.ts
+++ src/app/load-workout/load-workout.component.ts
@@ -3,17 +3,21 @@ import { ModelService } from '../model.service';
 import { Workout } from '../classes/Workout';
 
 @Component({
-	selector: 'app-load-workout',
-	templateUrl: './load-workout.component.html',
-	styleUrls: ['./load-workout.component.less']
+  selector: 'app-load-workout',
+  templateUrl: './load-workout.component.html',
+  styleUrls: ['./load-workout.component.less']
 })
 export class LoadWorkoutComponent implements OnInit {
-	workouts: Workout[] = [];
+  workouts: Workout[] = [];
 
-	constructor(private model: ModelService) { }
+  constructor(private model: ModelService) { }
 
-	ngOnInit() {
-		this.workouts = this.model.getSavedWorkouts();
-	}
+  ngOnInit() {
+    this.workouts = this.model.getSavedWorkouts();
+  }
+
+  onActivateWorkout (workout: Workout) {
+    this.model.setWorkout(workout);
+  }
 
 }
